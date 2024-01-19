42a140ba0cae7f6144481e9f41c638d8f9a2cc22
mnlvgan <150302503+mnlvgan@users.noreply.github.com>
2024-01-20 01:09:46 +0800

    added on scroll animations

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+//on scroll animation
+function isPartiallyInViewport(element, offsetPercentage = 0) {
+  const rect = element.getBoundingClientRect();
+  const threshold = offsetPercentage / 100;
+
+  return (
+    rect.top <= window.innerHeight * (1 - threshold) &&
+    rect.bottom >= window.innerHeight * threshold
+  );
+}
+
+function handleScrollAnimation() {
+  const elements = document.querySelectorAll(".animate");
+
+  elements.forEach((element) => {
+    if (isPartiallyInViewport(element, 45)) {
+      element.classList.add("animate-in");
+    }
+  });
+}
+
+window.addEventListener("scroll", handleScrollAnimation);
+handleScrollAnimation();
